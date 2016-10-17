f22278e573f52d3b72fb2d387efff6cee7873131
Brandon Rodriguez <brodriguez8774@gmail.com>
2016-10-17 12:15:17 -0400

    Setup general website layout, currently in greyscale

--- WebSite/index.php
+++ WebSite/index.php
@@ -8,22 +8,27 @@
     include "templates/nav.php";
   ?>
   <main>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum urna et ante luctus lacinia in hendrerit purus. Nulla in tortor rutrum est congue fermentum. Maecenas laoreet at ex non vestibulum. Fusce aliquam tincidunt ultrices. Duis in finibus neque. Quisque velit mi, ultrices sed viverra sed, accumsan sed metus. Nam a dapibus augue. Vestibulum sit amet tortor suscipit, vulputate nisi vel, auctor dui. Integer ullamcorper nisl eget elit rutrum porttitor sed eu risus. Quisque lacinia mollis massa non porttitor. Nullam ut consequat diam, vel vulputate felis. Praesent ullamcorper tellus nec libero iaculis tempor.
-    </p>
-    <p>
-      Praesent convallis hendrerit nunc sit amet condimentum. Curabitur libero nisl, vestibulum vel aliquet in, tincidunt non mauris. In varius aliquam sollicitudin. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam neque ex, porta vel dictum a, eleifend eget tortor. Proin faucibus elit ut tempor tempus. Fusce gravida vulputate lacus, eget condimentum nisl sagittis ut. Etiam dignissim, turpis eget condimentum imperdiet, risus elit convallis urna, at sagittis diam turpis sit amet elit. Nunc mollis massa a quam fringilla laoreet. Praesent porttitor tempus ornare.
-    </p>
-    <p>
-      Suspendisse potenti. Maecenas id mattis erat. Etiam nec sem quis neque accumsan dapibus vel vitae tortor. Vestibulum nec est blandit, sodales nisl id, rhoncus lacus. Nam vitae viverra tortor. Proin justo sapien, porttitor a laoreet at, tristique sed arcu. Etiam lobortis, velit vitae aliquet gravida, augue augue vulputate justo, ut eleifend mi lacus non ligula.
-    </p>
-    <p>
-      Maecenas molestie efficitur posuere. Curabitur lacinia felis in lorem sodales lobortis. Aliquam eleifend libero vitae congue convallis. Cras auctor ligula aliquam purus accumsan, gravida feugiat tortor ultrices. Aliquam erat volutpat. Nam porta nisl eget quam interdum tempus. In efficitur massa ultrices massa tincidunt commodo efficitur a justo. Sed et est quis velit porta efficitur id pharetra neque. Sed venenatis, velit et placerat tempus, purus elit imperdiet purus, nec pharetra elit metus vel nunc. In vitae urna augue. Nulla consectetur erat metus, id cursus dui consectetur sed. Duis leo nulla, scelerisque vitae neque sit amet, tempor blandit arcu. Integer eget lacinia risus, sit amet aliquam nisl.
-    </p>
-    <p>
-      Suspendisse condimentum eros quis ipsum consectetur, ac gravida sapien aliquam. Nam venenatis id ipsum quis pellentesque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse potenti. Vivamus mattis nisl velit, id interdum ipsum pharetra quis. Curabitur rutrum luctus laoreet. Quisque a erat eget sapien ornare sollicitudin quis nec lectus. Morbi luctus leo nec nunc fermentum rutrum. Etiam nulla purus, suscipit sed enim nec, posuere tempus nunc. Vestibulum nec bibendum tellus. Praesent ornare massa id justo faucibus mattis in in purus. Suspendisse odio nisi, consequat tempor tortor ut, imperdiet viverra nisi. Vestibulum est leo, rutrum at sem eu, auctor pretium sapien. Curabitur ultricies arcu eget vulputate finibus.
-    </p>
+    <div class="placeholder"></div>
+    <div id="content">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum urna et ante luctus lacinia in hendrerit purus. Nulla in tortor rutrum est congue fermentum. Maecenas laoreet at ex non vestibulum. Fusce aliquam tincidunt ultrices. Duis in finibus neque. Quisque velit mi, ultrices sed viverra sed, accumsan sed metus. Nam a dapibus augue. Vestibulum sit amet tortor suscipit, vulputate nisi vel, auctor dui. Integer ullamcorper nisl eget elit rutrum porttitor sed eu risus. Quisque lacinia mollis massa non porttitor. Nullam ut consequat diam, vel vulputate felis. Praesent ullamcorper tellus nec libero iaculis tempor.
+      </p>
+      <p>
+        Praesent convallis hendrerit nunc sit amet condimentum. Curabitur libero nisl, vestibulum vel aliquet in, tincidunt non mauris. In varius aliquam sollicitudin. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam neque ex, porta vel dictum a, eleifend eget tortor. Proin faucibus elit ut tempor tempus. Fusce gravida vulputate lacus, eget condimentum nisl sagittis ut. Etiam dignissim, turpis eget condimentum imperdiet, risus elit convallis urna, at sagittis diam turpis sit amet elit. Nunc mollis massa a quam fringilla laoreet. Praesent porttitor tempus ornare.
+      </p>
+      <p>
+        Suspendisse potenti. Maecenas id mattis erat. Etiam nec sem quis neque accumsan dapibus vel vitae tortor. Vestibulum nec est blandit, sodales nisl id, rhoncus lacus. Nam vitae viverra tortor. Proin justo sapien, porttitor a laoreet at, tristique sed arcu. Etiam lobortis, velit vitae aliquet gravida, augue augue vulputate justo, ut eleifend mi lacus non ligula.
+      </p>
+      <p>
+        Maecenas molestie efficitur posuere. Curabitur lacinia felis in lorem sodales lobortis. Aliquam eleifend libero vitae congue convallis. Cras auctor ligula aliquam purus accumsan, gravida feugiat tortor ultrices. Aliquam erat volutpat. Nam porta nisl eget quam interdum tempus. In efficitur massa ultrices massa tincidunt commodo efficitur a justo. Sed et est quis velit porta efficitur id pharetra neque. Sed venenatis, velit et placerat tempus, purus elit imperdiet purus, nec pharetra elit metus vel nunc. In vitae urna augue. Nulla consectetur erat metus, id cursus dui consectetur sed. Duis leo nulla, scelerisque vitae neque sit amet, tempor blandit arcu. Integer eget lacinia risus, sit amet aliquam nisl.
+      </p>
+      <p>
+        Suspendisse condimentum eros quis ipsum consectetur, ac gravida sapien aliquam. Nam venenatis id ipsum quis pellentesque. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse potenti. Vivamus mattis nisl velit, id interdum ipsum pharetra quis. Curabitur rutrum luctus laoreet. Quisque a erat eget sapien ornare sollicitudin quis nec lectus. Morbi luctus leo nec nunc fermentum rutrum. Etiam nulla purus, suscipit sed enim nec, posuere tempus nunc. Vestibulum nec bibendum tellus. Praesent ornare massa id justo faucibus mattis in in purus. Suspendisse odio nisi, consequat tempor tortor ut, imperdiet viverra nisi. Vestibulum est leo, rutrum at sem eu, auctor pretium sapien. Curabitur ultricies arcu eget vulputate finibus.
+      </p>
+    </div>
+    <div class="placeholder"></div>
   </main>
+
   <?php
     include "templates/footer.php";
   ?>
--- WebSite/templates/nav.php
+++ WebSite/templates/nav.php
@@ -2,6 +2,11 @@
   <ul>
     <li><a href="index.php">Home</a></li>
     <li><a href="about.php">About</a></li>
+    <li>Menu<ul>
+      <li><a href="menu/coffee.php">Coffee</a></li>
+      <li><a href="menu/tea.php">Tea</a></li>
+      <li><a href="menu/bakery.php">Bakery</a></li>
+    </ul></li>
     <li><a href="contact.php">Contact Us</a></li>
   </ul>
 </nav>
