bee71f502aa2c7c0ff1da3601fae28ae6cde1b15
Brandon Rodriguez <brodriguez8774@gmail.com>
2016-11-23 01:29:15 -0500

    Make menu item images not squish on smaller screens
    Still needs general css work though.

--- WebSite/templates/menuItem.inc.php
+++ WebSite/templates/menuItem.inc.php
@@ -9,7 +9,9 @@
             echo "<p> $ " . $foodRow['price'] . "</p>";
           echo "</div>";
           echo "<div class='menu-item-body'>";
-            echo "<img src='.." . $foodRow['img_dir'] . "' alt='" . $foodRow['name'] . "'>";
+            echo "<div>";
+              echo "<img src='.." . $foodRow['img_dir'] . "' alt='" . $foodRow['name'] . "'>";
+            echo "</div>";
             echo "<p>" . $foodRow['description'] . "</p>";
           echo "</div>";
         echo "</div>";
