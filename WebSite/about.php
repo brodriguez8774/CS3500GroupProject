b284856fd52b089c7c4e233149a9008dc50a452e
Brandon Rodriguez <brodriguez8774@gmail.com>
2016-11-23 18:47:24 -0500

    Create about page

--- WebSite/about.php
+++ WebSite/about.php
@@ -10,7 +10,28 @@
   <main>
     <div class="placeholder"></div>
     <div id="content">
-
+      <h3>About Us</h3>
+      <div class="logo-img">
+        <img src="static/images/CoffeeLogo2.png" alt="CoffeeLogo">
+      </div>
+      <p>
+        Coffee Shop brand coffee stores are the premier something or other. We love coffee, and something something convincing selling stuff here.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sit amet convallis nisi, sit amet tincidunt mi. Donec vehicula iaculis nisi non iaculis. Donec vulputate ultrices leo vitae lobortis. Nullam vitae lectus laoreet, rutrum quam vel, imperdiet ex. Etiam auctor ultricies elit, non pretium risus aliquet non. Proin ac ex tempus, tempus dui commodo, malesuada libero. Nullam malesuada nibh sit amet ipsum pulvinar, ac scelerisque sapien volutpat. Aliquam ac ullamcorper dui, non facilisis lectus.
+      </p>
+      <p>
+        Morbi hendrerit, quam non rhoncus facilisis, justo tellus ultrices lorem, et tristique lectus arcu sit amet nulla. Mauris pulvinar justo ut magna imperdiet auctor. Phasellus id arcu id elit dapibus dignissim. Proin id pulvinar odio. Pellentesque gravida fringilla blandit. Fusce et lobortis lorem. Praesent tincidunt felis sed erat dictum, at pellentesque orci lobortis. Proin laoreet, elit quis fringilla cursus, tellus quam vulputate mauris, quis facilisis dui nibh facilisis sem. Curabitur lectus orci, volutpat consequat nibh efficitur, suscipit facilisis ligula. Sed ut ligula dui.
+      </p>
+      <p>
+        Sed varius laoreet ligula. Nullam id felis ut magna ultricies facilisis. Praesent gravida convallis massa in dictum. Etiam ac dignissim turpis. Sed suscipit consectetur lorem. Mauris luctus ligula a sem ultricies fermentum. Suspendisse tempus interdum ex eu auctor.
+      </p>
+      <p>
+        Maecenas a elit nec ipsum dignissim iaculis. Phasellus eget dignissim orci. Aliquam dignissim egestas turpis vitae scelerisque. Nulla at condimentum erat, vel consectetur mauris. Proin felis lorem, tempor ut magna et, pulvinar tincidunt dolor. Nullam arcu massa, maximus sed massa vitae, elementum lacinia nibh. In blandit nisl non nisl aliquam, quis aliquet purus malesuada.
+      </p>
+      <p>
+        Suspendisse bibendum risus tellus, ut faucibus augue ullamcorper eu. Nam in volutpat metus, eu dapibus mi. Maecenas cursus nisl non sodales mollis. Mauris interdum fermentum lorem commodo pulvinar.
+      </p>
     </div>
     <div class="placeholder"></div>
   </main>
