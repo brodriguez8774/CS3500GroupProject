f86bca5063fd219b2fd792739295d869454f601d
Brandon Rodriguez <brodriguez8774@gmail.com>
2016-11-23 02:11:53 -0500

    Fix menu nav dropdown

--- WebSite/templates/nav.php
+++ WebSite/templates/nav.php
@@ -2,10 +2,12 @@
   <ul>
     <li><a href="<?php echo $baseUrl; ?>index.php">Home</a></li>
     <li><a href="<?php echo $baseUrl; ?>about.php">About</a></li>
-    <li>Menu<ul>
-      <li><a href="<?php echo $baseUrl; ?>menu/coffee.php">Coffee</a></li>
-      <li><a href="<?php echo $baseUrl; ?>menu/tea.php">Tea</a></li>
-      <li><a href="<?php echo $baseUrl; ?>menu/bakery.php">Bakery</a></li>
+    <li class="drop">Menu<ul>
+      <div>
+        <li><a href="<?php echo $baseUrl; ?>menu/coffee.php">Coffee</a></li>
+        <li><a href="<?php echo $baseUrl; ?>menu/tea.php">Tea</a></li>
+        <li><a href="<?php echo $baseUrl; ?>menu/bakery.php">Bakery</a></li>
+      </div>
     </ul></li>
     <li><a href="<?php echo $baseUrl; ?>contact.php">Contact Us</a></li>
   </ul>
